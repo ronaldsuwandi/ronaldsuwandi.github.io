b35223225c01c9d1aabe1b2d5708cee34b0b853c
Ronald Suwandi <ronald@ronaldsuwandi.com>
2016-04-28 08:51:45 +0800

    Initial version using handlebars template

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,10 @@
 var gulp = require('gulp');
+var handlebars = require('gulp-compile-handlebars');
 var webserver = require('gulp-webserver');
 var less = require('gulp-less');
 var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var decache = require('decache');
 
 gulp.task('webserver', function() {
   gulp.src(['target'])
@@ -13,13 +16,13 @@ gulp.task('webserver', function() {
 
 gulp.task('less', function () {
   return gulp.src('src/less/**/*.less')
-    .pipe(less({
-    }))
+    .pipe(less({}))
     .pipe(gulp.dest('target/css'));
 });
 
 gulp.task('copy', function() {
-  return gulp.src(['src/**/*', '!src/lib{,/**}', '!src/less{,/**}', '!src/js{,/**}'])
+  return gulp.src(['src/**/*', '!src/**/*.hbs', '!src/data{,/**}', '!src/lib{,/**}',
+                   '!src/less{,/**}', '!src/js{,/**}'])
     .pipe(gulp.dest('./target'));
 });
 
@@ -34,13 +37,24 @@ gulp.task('uglify', function() {
     .pipe(gulp.dest('target/js'));
 });
 
+gulp.task('template', function() {
+  decache('./src/data/data.js');
+  var templateData = require('./src/data/data.js');
+
+  gulp.src('src/index.hbs')
+    .pipe(handlebars(templateData, {}))
+    .pipe(rename('index.html'))
+    .pipe(gulp.dest('target'));
+})
+
 gulp.task('watch', ['compile'], function() {
   gulp.watch(['src/*', 'src/img/**', 'src/lib/**'], ['copy']);
-  gulp.watch('src/lib/**', ['copy-lib'])
+  gulp.watch(['src/**/*.hbs', 'src/data/**'], ['template']);
+  gulp.watch('src/lib/**', ['copy-lib']);
   gulp.watch('src/less/**', ['less']);
   gulp.watch('src/js/**', ['uglify']);
 });
 
-gulp.task('compile', ['less', 'copy-lib', 'copy', 'uglify']);
+gulp.task('compile', ['less', 'copy-lib', 'copy', 'template', 'uglify']);
 
 gulp.task('default', ['watch', 'webserver']);
---
+++ src/data/data.js
@@ -0,0 +1,12 @@
+module.exports = {
+  works: [{
+    icon: '/img/works/nn.svg',
+    title: 'Neural Networks library',
+    subtitle: 'github',
+    link: 'http://github.com/ronaldsuwandi/neuralnetworks',
+    period: 'April 2016 – Present',
+    description: `<p>Clojure-based neural networks library. Some more exciting sentences here.</p>
+
+    blah blah`
+  }]
+}
