a002bb321a69d0e8ef68d5862860ea0c476223b9
Ronald Suwandi <ronald@ronaldsuwandi.com>
2019-01-23 11:18:17 +0800

    Updated data

--- src/data/data.js
+++ src/data/data.js
@@ -29,9 +29,9 @@ module.exports = {
       period: 'June 2018 – Present',
       description: `
 <ul>
+<li>Developed backend API using Java, TypeScript, Spring Boot and MongoD</li>
 <li>Created and manage Kubernetes cluster on both local server through kubeadm and Google Cloud through GKE using Helm as the package manager and deployment to various environments</li>
 <li>Setup CI/CD environment using Bamboo for all projects</li>
-<li>Developed backend application using TypeScript and MongoDB</li>
 <li>Containerized backend applications using Docker</li>
 </ul>
 `
