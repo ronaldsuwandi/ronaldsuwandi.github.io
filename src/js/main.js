be69b6f548d870dec62ac440166d2208b0ebb40a
Ronald Suwandi <ronald@ronaldsuwandi.com>
2026-08-11 17:31:41 +0800

    Removed old velocity.js dependency

--- src/js/main.js
+++ src/js/main.js
@@ -2,9 +2,9 @@ var experiences = document.getElementsByClassName('experiences')[0];
 var main = document.getElementsByClassName('main')[0];
 
 document.getElementsByClassName('scroll-to-experiences')[0].onclick = function(e) {
-  Velocity(experiences, 'scroll', {duration: 1000, easing: 'ease'});
+  experiences.scrollIntoView({behavior: 'smooth'});
 };
 
 document.getElementsByClassName('scroll-to-main')[0].onclick = function(e) {
-  Velocity(main, 'scroll', {duration: 1000, easing: 'ease'});
+  main.scrollIntoView({behavior: 'smooth'});
 };
